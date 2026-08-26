87df3255752b4e7284dd79039ab898a8259dd837
Arath Campos <arcmp0130@gmail.com>
2026-08-26 14:20:31 +0000

    feat: creating new ui elements

--- projects/003_gui.py
+++ projects/003_gui.py
@@ -2,25 +2,37 @@ import tkinter as tk
 import random
 
 def getRandom():
-  print(random.randint(1, 100))
+  lbl.config(text=random.randint(1, 100))
   return
 
+def greet():
+  name = entry1.get().strip()
+  if not name:
+    name = "Arath"
+  lbl.config(text=f"Hello, {name}")
+
+
 root = tk.Tk()
+root.geometry("400x300")
 
-tk.Label(root, text="Inicio").grid(row=0, column=0)
-tk.Label(root, text="Incremento").grid(row=1, column=0)
-tk.Label(root, text="Final").grid(row=2, column=0)
+tk.Label(root, text="Name").grid(pady=20, row=0, column=0)
+tk.Label(root, text="Incremento").grid(pady=20, row=1, column=0)
+tk.Label(root, text="Final").grid(pady=20, row=2, column=0)
 
+lbl = tk.Label(root)
 entry1 = tk.Entry(root)
 entry2 = tk.Entry(root)
 entry3 = tk.Entry(root)
 button1 = tk.Button(root, text="Random", command=getRandom)
 button2 = tk.Button(root, text="Close app", command=root.destroy)
+button3 = tk.Button(root, text="Greet", command=greet)
 
-entry1.grid(row=0, column=1)
-entry2.grid(row=1, column=1)
-entry3.grid(row=2, column=1)
-button1.grid(row=3, column=0)
-button2.grid(row=3, column=1)
+entry1.grid(pady=10, row=0, column=1)
+entry2.grid(pady=10, row=1, column=1)
+entry3.grid(pady=10, row=2, column=1)
+button1.grid(pady=10, row=3, column=0)
+button2.grid(pady=10, row=3, column=1)
+button3.grid(pady=10, row=3, column=2)
+lbl.grid(pady=10, row=4, column=1)
 
 root.mainloop()
